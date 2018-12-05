d02a29b9476c96332ed0037f4cd1db5428e38576
karthikreddy <ka452@njit.edu>
2018-12-04 19:25:27 -0500

    Created flash for showing messages for errors and status messages

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Profile;
+use App\User;
+
 
 class ProfileController extends Controller
 {
